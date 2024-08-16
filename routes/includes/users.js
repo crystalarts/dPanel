2d000287497dd4f5493d45119a1c5aba538e3bfe
discordxts <burzynsonek@gmail.com>
2024-08-16 21:14:13 +0200

    refactor: Update user management form and table styles

--- routes/includes/users.js
+++ routes/includes/users.js
@@ -1,32 +1,55 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../../database/mysql-promise");
+const bcrypt = require("bcrypt");
 
-router.post('/add-user', (req, res) => {
-  const { name, email, password, verified } = req.body;
-  db.query('INSERT INTO user (name, email, password, verify) VALUES (?, ?, ?, ?)', 
-  [name, email, password, verified === '1'], (err) => {
-    if (err) throw err;
+router.post('/add-user', async (req, res) => {
+  try {
+    const { name, email, password, verified } = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await db.query('INSERT INTO user (name, email, password, verify) VALUES (?, ?, ?, ?)', 
+    [name, email, hashedPassword, verified === '1']);
+    
     res.redirect('/');
-  });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
-router.post('/edit-user/:id', (req, res) => {
-  const { id } = req.params;
-  const { name, email, password, verified } = req.body;
-  db.query('UPDATE user SET name = ?, email = ?, password = ?, verify = ? WHERE id = ?', 
-  [name, email, password, verified === '1', id], (err) => {
-    if (err) throw err;
+router.post('/edit-user/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, email, password, verified } = req.body;
+
+    let hashedPassword;
+    if (password) {
+      hashedPassword = await bcrypt.hash(password, 10);
+    }
+
+    await db.query('UPDATE user SET name = ?, email = ?, password = ?, verify = ? WHERE id = ?', 
+    [name, email, hashedPassword || password, verified === '1', id]);
+    
     res.redirect('/');
-  });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
-router.post('/delete-user/:id', (req, res) => {
-  const { id } = req.params;
-  db.query('DELETE FROM user WHERE id = ?', [id], (err) => {
-    if (err) throw err;
+router.post('/delete-user/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    await db.query('DELETE FROM user WHERE id = ?', [id]);
+    
     res.redirect('/');
-  });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 module.exports = router;
